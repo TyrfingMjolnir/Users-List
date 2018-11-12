862b7df93214920ee08ecf51aeb9fe4f5e28f1b3
Tyrfing Mjølner <4lphamal3@gmail.com>
2018-11-12 08:00:51 +0100

    Reformatting
    Reformatting CTRL + I

--- Dem Users Doe/UsersVC.swift
+++ Dem Users Doe/UsersVC.swift
@@ -9,34 +9,35 @@
 import UIKit
 
 class UsersVC: UITableViewController {
-
-    let temporaryUsers = ["Jannie", "Jessica", "Kyle L.", "Kyle N.", "Steven"]
+  
+  let temporaryUsers = ["Jannie", "Jessica", "Kyle L.", "Kyle N.", "Steven"]
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    print("ohai")
+  }
+  
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return temporaryUsers.count
+  }
+  
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("ohai")
-    }
-
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return temporaryUsers.count
+    let cell = UITableViewCell()
+    let user = temporaryUsers[indexPath.row]
+    
+    cell.textLabel?.text = user
+    if( indexPath.row % 3 == 0 ) {
+      cell.backgroundColor = UIColor( red: 0.5, green: 1.0, blue: 0.75, alpha: 1.0 )
+    } else if( indexPath.row % 3 == 1 ) {
+      cell.backgroundColor = UIColor( red: 0.5, green: 1.0, blue: 0.75, alpha: 0.7 )
+    } else {
+      cell.backgroundColor = UIColor( red: 0.5, green: 1.0, blue: 0.75, alpha: 0.5 )
     }
     
+    return cell
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = UITableViewCell()
-        let user = temporaryUsers[indexPath.row]
-        
-        cell.textLabel?.text = user
-        if( indexPath.row % 3 == 0 ) {
-          cell.backgroundColor = UIColor( red: 0.5, green: 1.0, blue: 0.75, alpha: 1.0 )
-        } else if( indexPath.row % 3 == 1 ) {
-          cell.backgroundColor = UIColor( red: 0.5, green: 1.0, blue: 0.75, alpha: 0.7 )
-        } else {
-          cell.backgroundColor = UIColor( red: 0.5, green: 1.0, blue: 0.75, alpha: 0.5 )
-        }
-
-        return cell
-    }
+  }
 }
